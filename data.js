bec608204637eed472447e98c0662520cb36543d
github-action-benchmark <github@users.noreply.github.com>
2024-09-20 02:51:28 +0000

    add lmbench/mem_copy_bw (customBiggerIsBetter) benchmark result for c5009e38f12810141f1ece9d64544af59d68a3d3

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726800542524,
+  "lastUpdate": 1726800688060,
   "repoUrl": "https://github.com/asterinas/asterinas",
   "entries": {
     "sysbench/thread_lat": [
@@ -22946,6 +22946,43 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory copy bandwidth on Asterinas",
             "value": "6453.55",
+            "unit": "MB/s",
+            "extra": "aster_result"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Jianfeng Jiang",
+            "username": "StevenJiang1110",
+            "email": "jiangjianfeng.jjf@antgroup.com"
+          },
+          "committer": {
+            "name": "Tate, Hongliang Tian",
+            "username": "tatetian",
+            "email": "tatetian@gmail.com"
+          },
+          "id": "c5009e38f12810141f1ece9d64544af59d68a3d3",
+          "message": "Add pipe/short_rw test\n\nCo-authored-by: Ruihan Li <lrh2000@pku.edu.cn>",
+          "timestamp": "2024-09-11T09:50:56Z",
+          "url": "https://github.com/asterinas/asterinas/commit/c5009e38f12810141f1ece9d64544af59d68a3d3"
+        },
+        "date": 1726800682487,
+        "tool": "customBiggerIsBetter",
+        "title": "[Memory] The bandwidth of copying integers",
+        "description": "bw_mem fcp",
+        "display": true,
+        "benches": [
+          {
+            "name": "Average memory copy bandwidth on Linux",
+            "value": "6418.83",
+            "unit": "MB/s",
+            "extra": "linux_result"
+          },
+          {
+            "name": "Average memory copy bandwidth on Asterinas",
+            "value": "6423.13",
             "unit": "MB/s",
             "extra": "aster_result"
           }
