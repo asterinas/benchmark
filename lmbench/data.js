759dddf082bfbb5ab4b074be5688ca1de337cbf8
github-action-benchmark <github@users.noreply.github.com>
2024-11-08 03:06:32 +0000

    add mem_mmap_bw (customBiggerIsBetter) benchmark result for 4c80367bc8d03430a667d7c31cd71c1cee63d3a2

--- lmbench/data.js
+++ lmbench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1731035044404,
+  "lastUpdate": 1731035192449,
   "repoUrl": "https://github.com/asterinas/asterinas",
   "entries": {
     "mem_pagefault_lat": [
@@ -6439,6 +6439,43 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average mmap bandwidth on Asterinas",
             "value": "26533.11",
+            "unit": "MB/s",
+            "extra": "aster_result"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "jiangjianfeng",
+            "username": "StevenJiang1110",
+            "email": "jiangjianfeng.jjf@antgroup.com"
+          },
+          "committer": {
+            "name": "Tate, Hongliang Tian",
+            "username": "tatetian",
+            "email": "tatetian@gmail.com"
+          },
+          "id": "4c80367bc8d03430a667d7c31cd71c1cee63d3a2",
+          "message": "Fix CI lint error",
+          "timestamp": "2024-11-08T02:52:01Z",
+          "url": "https://github.com/asterinas/asterinas/commit/4c80367bc8d03430a667d7c31cd71c1cee63d3a2"
+        },
+        "date": 1731035179497,
+        "tool": "customBiggerIsBetter",
+        "title": "[Memory] The bandwidth of mmap",
+        "description": "bw_mmap",
+        "display": true,
+        "benches": [
+          {
+            "name": "Average mmap bandwidth on Linux",
+            "value": "24765.70",
+            "unit": "MB/s",
+            "extra": "linux_result"
+          },
+          {
+            "name": "Average mmap bandwidth on Asterinas",
+            "value": "27164.08",
             "unit": "MB/s",
             "extra": "aster_result"
           }
