1766ee0a037050a10a7a8bc8d4d5731a49581c40
github-action-benchmark <github@users.noreply.github.com>
2025-02-21 01:59:58 +0000

    add ext2_benchmarks_updates_between (customSmallerIsBetter) benchmark result for d74d126e6410779610d2003fefb570ca8b04b621

--- sqlite/data.js
+++ sqlite/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1740103123829,
+  "lastUpdate": 1740103198828,
   "repoUrl": "https://github.com/asterinas/asterinas",
   "entries": {
     "ext2_deletes_between": [
@@ -41359,6 +41359,43 @@ window.BENCHMARK_DATA = {
           {
             "name": "The speed of updates-between on Asterinas (Ext2)",
             "value": "2.109",
+            "unit": "second",
+            "extra": "aster_result"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Zhang Junyang",
+            "username": "junyang-zh",
+            "email": "junyang@stu.pku.edu.cn"
+          },
+          "committer": {
+            "name": "Tate, Hongliang Tian",
+            "username": "tatetian",
+            "email": "tatetian@gmail.com"
+          },
+          "id": "d74d126e6410779610d2003fefb570ca8b04b621",
+          "message": "Enhance OSDK profile stability for SMP",
+          "timestamp": "2025-02-20T09:47:15Z",
+          "url": "https://github.com/asterinas/asterinas/commit/d74d126e6410779610d2003fefb570ca8b04b621"
+        },
+        "date": 1740103139272,
+        "tool": "customSmallerIsBetter",
+        "title": "[Ext2] The updates-between result of sqlite speedtest",
+        "description": "sqlite-speed-test: 10000 UPDATES, numeric BETWEEN, indexed",
+        "display": false,
+        "benches": [
+          {
+            "name": "The speed of updates-between on Linux (Ext2)",
+            "value": "1.968",
+            "unit": "second",
+            "extra": "linux_result"
+          },
+          {
+            "name": "The speed of updates-between on Asterinas (Ext2)",
+            "value": "2.072",
             "unit": "second",
             "extra": "aster_result"
           }
