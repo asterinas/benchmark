8dea9c0635f95b985fc8a635c1eb7521962bb47f
github-action-benchmark <github@users.noreply.github.com>
2025-02-27 03:37:56 +0000

    add cpu_lat (customSmallerIsBetter) benchmark result for 9b8c6b5aa9be4124bdfdaba96b2d2185d9fcdd85

--- sysbench/data.js
+++ sysbench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1740556402686,
+  "lastUpdate": 1740627476378,
   "repoUrl": "https://github.com/asterinas/asterinas",
   "entries": {
     "cpu_lat": [
@@ -8744,6 +8744,43 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average Execution Time per CPU on Linux",
             "value": "3.85",
+            "unit": "ms",
+            "extra": "linux_result"
+          },
+          {
+            "name": "Average Execution Time per CPU on Asterinas",
+            "value": "3.85",
+            "unit": "ms",
+            "extra": "aster_result"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Fabing Li",
+            "username": "grief8",
+            "email": "lifabing.lfb@antgroup.com"
+          },
+          "committer": {
+            "name": "Tate, Hongliang Tian",
+            "username": "tatetian",
+            "email": "tatetian@gmail.com"
+          },
+          "id": "9b8c6b5aa9be4124bdfdaba96b2d2185d9fcdd85",
+          "message": "Update the toolchain version in template",
+          "timestamp": "2025-02-27T03:18:18Z",
+          "url": "https://github.com/asterinas/asterinas/commit/9b8c6b5aa9be4124bdfdaba96b2d2185d9fcdd85"
+        },
+        "date": 1740627413868,
+        "tool": "customSmallerIsBetter",
+        "title": "[CPU] CPU performance",
+        "description": "sysbench cpu",
+        "display": true,
+        "benches": [
+          {
+            "name": "Average Execution Time per CPU on Linux",
+            "value": "3.84",
             "unit": "ms",
             "extra": "linux_result"
           },
