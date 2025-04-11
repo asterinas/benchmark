95d4168d1d8cd475c7e10ed132bece6f0b08b849
github-action-benchmark <github@users.noreply.github.com>
2025-04-11 06:49:12 +0000

    add ramfs_benchmarks_vacuum (customSmallerIsBetter) benchmark result for e9c2886a84698efd3493dcacf8f6563cde139c40

---
+++ tdx/sqlite/data.js
@@ -0,0 +1,45 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1744354152459,
+  "repoUrl": "https://github.com/asterinas/asterinas",
+  "entries": {
+    "ramfs_benchmarks_vacuum": [
+      {
+        "commit": {
+          "author": {
+            "name": "Fabian LI",
+            "username": "grief8",
+            "email": "lifabing1349212501@gmail.com"
+          },
+          "committer": {
+            "name": "Tate, Hongliang Tian",
+            "username": "tatetian",
+            "email": "tatetian@gmail.com"
+          },
+          "id": "e9c2886a84698efd3493dcacf8f6563cde139c40",
+          "message": "Fix platform definition",
+          "timestamp": "2025-04-10T08:27:21Z",
+          "url": "https://github.com/asterinas/asterinas/commit/e9c2886a84698efd3493dcacf8f6563cde139c40"
+        },
+        "date": 1744354144852,
+        "tool": "customSmallerIsBetter",
+        "title": "[Ramfs] The 'VACUUM' result of sqlite speedtest",
+        "description": "sqlite-speed-test: repacking database file into a minimal amount of disk space",
+        "display": false,
+        "benches": [
+          {
+            "name": "The speed of VACUUM on Linux (Ramfs)",
+            "value": "0.861",
+            "unit": "second",
+            "extra": "linux_result"
+          },
+          {
+            "name": "The speed of VACUUM on Asterinas (Ramfs)",
+            "value": "0.902",
+            "unit": "second",
+            "extra": "aster_result"
+          }
+        ]
+      }
+    ]
+  }
+}
