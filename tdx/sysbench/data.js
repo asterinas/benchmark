b6e4b9b679be762be7742ba21b4045f0e7624d5c
github-action-benchmark <github@users.noreply.github.com>
2025-04-15 11:59:18 +0000

    add cpu_lat (customSmallerIsBetter) benchmark result for 6ef74345bb5547ffbbdd9d8edac421256c4a2b5c

--- tdx/sysbench/data.js
+++ tdx/sysbench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744717967310,
+  "lastUpdate": 1744718358553,
   "repoUrl": "https://github.com/asterinas/asterinas",
   "entries": {
     "cpu_lat": [
@@ -66,6 +66,43 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average Execution Time per CPU on Linux",
             "value": "3.84",
+            "unit": "ms",
+            "extra": "linux_result"
+          },
+          {
+            "name": "Average Execution Time per CPU on Asterinas",
+            "value": "3.92",
+            "unit": "ms",
+            "extra": "aster_result"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Fabian LI",
+            "username": "grief8",
+            "email": "lifabing1349212501@gmail.com"
+          },
+          "committer": {
+            "name": "Tate, Hongliang Tian",
+            "username": "tatetian",
+            "email": "tatetian@gmail.com"
+          },
+          "id": "6ef74345bb5547ffbbdd9d8edac421256c4a2b5c",
+          "message": "Remove Benchmark Intel TDX badge temporarily",
+          "timestamp": "2025-04-11T03:36:33Z",
+          "url": "https://github.com/asterinas/asterinas/commit/6ef74345bb5547ffbbdd9d8edac421256c4a2b5c"
+        },
+        "date": 1744718351299,
+        "tool": "customSmallerIsBetter",
+        "title": "[CPU] CPU performance",
+        "description": "sysbench cpu",
+        "display": true,
+        "benches": [
+          {
+            "name": "Average Execution Time per CPU on Linux",
+            "value": "3.85",
             "unit": "ms",
             "extra": "linux_result"
           },
