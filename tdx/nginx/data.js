07203346729a4f5a6dd198ef6361a4a23238868e
github-action-benchmark <github@users.noreply.github.com>
2025-08-17 07:30:48 +0000

    add http_file8KB_bw (customBiggerIsBetter) benchmark result for 1452aab69c9d0bf5ada7ea8a93a49fd0badafcca

--- tdx/nginx/data.js
+++ tdx/nginx/data.js
@@ -1,7 +1,46 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1755415802292,
+  "lastUpdate": 1755415848806,
   "repoUrl": "https://github.com/asterinas/asterinas",
   "entries": {
+    "http_file8KB_bw": [
+      {
+        "commit": {
+          "author": {
+            "name": "Zhang Junyang",
+            "username": "junyang-zh",
+            "email": "junyang@stu.pku.edu.cn"
+          },
+          "committer": {
+            "name": "Tate, Hongliang Tian",
+            "username": "tatetian",
+            "email": "tatetian@gmail.com"
+          },
+          "id": "1452aab69c9d0bf5ada7ea8a93a49fd0badafcca",
+          "message": "Optimize the space for `TlbFlushOp`s",
+          "timestamp": "2025-08-14T14:08:56Z",
+          "url": "https://github.com/asterinas/asterinas/commit/1452aab69c9d0bf5ada7ea8a93a49fd0badafcca"
+        },
+        "date": 1755415803352,
+        "tool": "customBiggerIsBetter",
+        "title": "Nginx HTTP request performance (virtio-net, 8KB file size)",
+        "description": "ab -n 10000 -c 1 http://10.0.2.15:8080/8192bytes.html",
+        "display": true,
+        "benches": [
+          {
+            "name": "Average HTTP Bandwidth over virtio-net between Host Linux and Guest Linux",
+            "value": "5864.09",
+            "unit": "Requests per second",
+            "extra": "linux_result"
+          },
+          {
+            "name": "Average HTTP Bandwidth over virtio-net between Host Linux and Guest Asterinas",
+            "value": "3081.50",
+            "unit": "Requests per second",
+            "extra": "aster_result"
+          }
+        ]
+      }
+    ],
     "http_req10k_conc20_bw": [
       {
         "commit": {
